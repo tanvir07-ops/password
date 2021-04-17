fdb96892642f8c1dfe9193450da4b519826438e1
tanvir07-ops <tanvir9836859@gmail.com>
2021-04-17 08:02:34 -0700

    I commit this second time

--- password_generator/password_generator/urls.py
+++ password_generator/password_generator/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 
 from django.urls import path
 from generator import views
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
 
 
 urlpatterns = [
    path('', views.home, name="home"),
    path('password/', views.password, name="password"),
    path('about/', views.about, name="about"),
+   url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+   url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
   
 ]
